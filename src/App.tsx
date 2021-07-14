fad099f1e136913d6fd4c71fff94619e26f69d1a
royeradames <royeraadames@gmail.com>
2021-07-14 15:22:51 -0400

    📦add footer and header content to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import "./css/main.scss";
 
+// components
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
