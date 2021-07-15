db6e658c5e5acf6166a94a99529356cefc488d6a
royeradames <royeraadames@gmail.com>
2021-07-14 20:18:10 -0400

    📦add Hamburger and close icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { ReactComponent as Logo } from "../images/logo.svg";
+import { ReactComponent as Hamburger } from "../images/icons/hamburger.svg";
+import { ReactComponent as Close } from "../images/icons/close.svg";
 import { Link } from "react-router-dom";
 
 function Header() {
@@ -7,6 +9,8 @@ function Header() {
     <>
       <header className="header">
         <Logo className="header__logo" />
+        <Hamburger className="header__hamburger" />
+        <Close className="header__close" />
         <nav className="header__nav">
           <Link to="/">Home</Link>
           <Link to="/portfolio">Portfolio</Link>
