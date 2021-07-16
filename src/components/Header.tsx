80a63819d2ed1dd548ad3efef32e0e8d0460317e
royeradames <royeraadames@gmail.com>
2021-07-15 21:32:12 -0400

    📦hide and show menu links about content

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { ReactComponent as Logo } from "../images/logo.svg";
 import { ReactComponent as Hamburger } from "../images/icons/hamburger.svg";
 import { ReactComponent as Close } from "../images/icons/close.svg";
 import { Link } from "react-router-dom";
 
 function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const mobileMenuLogic = (
+    isMenuOpen: boolean,
+    setIsMenuOpen: React.Dispatch<React.SetStateAction<boolean>>
+  ) => {
+    // hide and show hambur, close icons and menu links
+    setIsMenuOpen(!isMenuOpen);
+  };
   return (
     <>
       <header className="header">
         <Logo className="header__logo" />
-        <Hamburger className="header__hamburger" />
-        <Close className="header__close" />
-        <nav className="header__nav">
-          <Link to="/">Home</Link>
-          <Link to="/portfolio">Portfolio</Link>
-          <Link to="/contact">Contact Me</Link>
+        <Hamburger
+          className={`header__hamburger ${isMenuOpen ? "header__hide" : ""}`}
+          onClick={() => mobileMenuLogic(isMenuOpen, setIsMenuOpen)}
+        />
+        <Close
+          className={`header__close ${isMenuOpen ? "" : "header__hide"}`}
+          onClick={() => mobileMenuLogic(isMenuOpen, setIsMenuOpen)}
+        />
+        <nav className={`header__nav ${isMenuOpen ? "" : "header__hide"}`}>
+          <Link className="header__nav-link" to="/">
+            Home
+          </Link>
+          <Link
+            className="header__nav-link header__nav-link-portfolio"
+            to="/portfolio"
+          >
+            Portfolio
+          </Link>
+          <Link className="header__nav-link" to="/contact">
+            Contact Me
+          </Link>
         </nav>
       </header>
     </>
