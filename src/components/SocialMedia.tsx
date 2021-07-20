f60c50c0d41159a40300920b85ba9e1adf35fcad
royeradames <royeraadames@gmail.com>
2021-07-20 16:49:16 -0400

    👌remove link import

--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { ReactComponent as Github } from "../images/icons/github.svg";
 import { ReactComponent as Twitter } from "../images/icons/twitter.svg";
 import { ReactComponent as LinkedIn } from "../images/icons/linkedin.svg";
