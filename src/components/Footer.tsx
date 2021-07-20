99658bdefad85db4a120a1d90d1c2dc8bee10fdb
royeradames <royeraadames@gmail.com>
2021-07-20 16:28:40 -0400

    📦import nav component for navigation menu

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,20 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { ReactComponent as Logo } from "../images/logo.svg";
-import { ReactComponent as Github } from "../images/icons/github.svg";
-import { ReactComponent as Twitter } from "../images/icons/twitter.svg";
-import { ReactComponent as Linkedin } from "../images/icons/linkedin.svg";
-
+import Nav from "./Nav";
+import SocialMedia from "./SocialMedia";
 function Footer() {
   return (
     <>
       <footer className="footer">
-        <Logo className="footer__logo" />
-        <nav className="footer__nav">
-          <Link to="/">Home</Link>
-          <Link to="/portfolio">Portfolio</Link>
-          <Link to="/contact">Contact Me</Link>
-        </nav>
+        <Link to="/">
+          <Logo className="footer__logo" />
+        </Link>
+        <Nav />
         <a href="https://github.com/royeradames">
           <Github className="footer__icon footer__github" />
         </a>
