5072532349d8a142a5a2aaac7b18eba096ea18b8
royeradames <royeraadames@gmail.com>
2021-08-02 13:18:27 -0400

    📦import react, link, viewportsize, data, and contact comp

---
+++ src/components/Portfolio.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import viewPortsSize from "../views";
+import projectSnipeDetails from "../projectSnipeDetails";
+// component
+import ContactMeArticle from "./ContactMeArticle";
