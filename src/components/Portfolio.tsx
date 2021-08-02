a284096cc60ca83ea9e95d3fc24b8eea36dd2c6b
royeradames <royeraadames@gmail.com>
2021-08-02 13:21:30 -0400

    📦start function that unpacks data

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -4,3 +4,14 @@ import viewPortsSize from "../views";
 import projectSnipeDetails from "../projectSnipeDetails";
 // component
 import ContactMeArticle from "./ContactMeArticle";
+
+const Portfolio = () => {
+  return (
+    <article className="portfolio">
+      {portfolioList(projectSnipeDetails)}
+      <ContactMeArticle page="portfolio" />
+    </article>
+  );
+};
+
+export default Portfolio;
