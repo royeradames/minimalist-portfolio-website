3f57e7c7cb182879d99a33944c61fdf8b1986b6d
royeradames <royeraadames@gmail.com>
2021-08-02 13:23:27 -0400

    📦add data structure to the incoming information

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -5,6 +5,14 @@ import projectSnipeDetails from "../projectSnipeDetails";
 // component
 import ContactMeArticle from "./ContactMeArticle";
 
+type DataStructure = {
+  id: number;
+  title: string;
+  message: string;
+  imgLinkMobile: string;
+  imgLinkTablet: string;
+  imgLinkDesktop: string;
+}[];
 const Portfolio = () => {
   return (
     <article className="portfolio">
