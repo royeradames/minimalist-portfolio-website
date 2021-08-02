d5564e80a4bc0b297f27bee737b8bebf7bf5834e
royeradames <royeraadames@gmail.com>
2021-08-02 15:10:28 -0400

    📦host project detail in its own file

---
+++ src/projectSnipeDetails.tsx
@@ -0,0 +1,40 @@
+const projectSnipeDetails = [
+  {
+    id: 1,
+    title: "manage",
+    message:
+      "This project required me to build a fully responsive landing page to the designs provided. I used HTML5, along with CSS Grid and JavaScript for the areas that required interactivity, such as the testimonial slider.",
+    imgLinkMobile: "./images/portfolio/mobile/image-portfolio-manage.jpg",
+    imgLinkTablet: "./images/portfolio/tablet/image-portfolio-manage.jpg",
+    imgLinkDesktop: "./images/portfolio/desktop/image-portfolio-manage.jpg",
+  },
+  {
+    id: 2,
+    title: "bookmark",
+    message:
+      "This project required me to build a fully responsive landing page to the designs provided. I used HTML5, along with CSS Grid and JavaScript for the areas that required interactivity, such as the features section.",
+    imgLinkMobile: "./images/portfolio/mobile/image-portfolio-bookmark.jpg",
+    imgLinkTablet: "./images/portfolio/tablet/image-portfolio-bookmark.jpg",
+    imgLinkDesktop: "./images/portfolio/desktop/image-portfolio-bookmark.jpg",
+  },
+  {
+    id: 3,
+    title: "insure",
+    message:
+      "This was a small project which mostly consisted of HTML and CSS. I built a fully-responsive landing page. The only JavaScript this project required was to enable the toggling of the mobile navigation.",
+    imgLinkMobile: "./images/portfolio/mobile/image-portfolio-insure.jpg",
+    imgLinkTablet: "./images/portfolio/tablet/image-portfolio-insure.jpg",
+    imgLinkDesktop: "./images/portfolio/desktop/image-portfolio-insure.jpg",
+  },
+  {
+    id: 4,
+    title: "fylo",
+    message:
+      "This project was built in pure HTML and CSS. I had mobile and desktop designs to work to and built it so that it was fully-responsive. I took a mobile-first approach and used modern CSS like Flexbox and Grid for layout purposes.",
+    imgLinkMobile: "./images/portfolio/mobile/image-portfolio-fylo.jpg",
+    imgLinkTablet: "./images/portfolio/tablet/image-portfolio-fylo.jpg",
+    imgLinkDesktop: "./images/portfolio/desktop/image-portfolio-fylo.jpg",
+  },
+];
+
+export default projectSnipeDetails;
