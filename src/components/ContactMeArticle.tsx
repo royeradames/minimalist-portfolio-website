b570f93c06f6c97fab041720fbee66d3c480a211
royeradames <royeraadames@gmail.com>
2021-08-02 15:13:32 -0400

    📦accept page name to give page specific classes

--- src/components/ContactMeArticle.tsx
+++ src/components/ContactMeArticle.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
-const ContactMeArticle = () => {
+const ContactMeArticle = (props: { page: string }) => {
   return (
-    <article className="home__contact-me contact-me">
-      <h2 className="home__title contact-me__title">
+    <article className={`contact-me ${props.page}__contact-me`}>
+      <h2 className="title home__title contact-me__title">
         Interested in doing a project together?
       </h2>
-      <div className="home__horizontal-line contact-me__horizontal-line" />
-      <Link to="contact" className="contact-me__button">
+      <div className="horizontal-line contact-me__horizontal-line" />
+      <Link to="/contact">
         <button className="secondary-button secondary-normal ">
           Contact Me
         </button>
