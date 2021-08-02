de458910fd73f1fa568a2cbfff4fe8c0a03baeaa
royeradames <royeraadames@gmail.com>
2021-08-02 14:29:58 -0400

    📦extract project data into it's own file

---
+++ src/projectsDetail.tsx
@@ -0,0 +1,80 @@
+const projectData = [
+  {
+    id: 1,
+    heroMobile: "/images/detail/mobile/image-manage-hero.jpg",
+    heroTablet: "/images/detail/tablet/image-manage-hero.jpg",
+    heroDesktop: "/images/detail/desktop/image-manage-hero.jpg",
+    title: "Manage",
+    intro:
+      "This project required me to build a fully responsive landing page to the designs provided. I used HTML5, along with CSS Grid and JavaScript for the areas that required interactivity, such as the testimonial slider.",
+    jobTitle: "Interaction Design / Front End Development",
+    techStack: "HTML / CSS / JS",
+    background:
+      "This project was a front-end  challenge from Frontend Mentor. It’s a platform that enables you to practice building websites to a design and project brief. Each challenge includes mobile and desktop designs to show how the website should look at different screen sizes. Creating these projects has helped me refine my workflow and solve real-world coding problems. I’ve learned something new with each project, helping me to improve and adapt my style.",
+    imgMobilePreview: "/images/detail/mobile/image-manage-preview-1.jpg",
+    imgMobile2Preview: "/images/detail/mobile/image-manage-preview-2.jpg",
+    imgTabletPreview: "/images/detail/tablet/image-manage-preview-2.jpg",
+    imgTablet2Preview: "/images/detail/tablet/image-manage-preview-2.jpg",
+    imgDesktopPreview: "/images/detail/desktop/image-manage-preview-2.jpg",
+    imgDesktop2Preview: "/images/detail/desktop/image-manage-preview-2.jpg",
+  },
+  {
+    id: 2,
+    heroMobile: "/images/detail/mobile/image-bookmark-hero.jpg",
+    heroTablet: "/images/detail/tablet/image-bookmark-hero.jpg",
+    heroDesktop: "/images/detail/desktop/image-bookmark-hero.jpg",
+    title: "Bookmark",
+    intro:
+      "This project required me to build a fully responsive landing page to the designs provided. I used HTML5, along with CSS Grid and JavaScript for the areas that required interactivity, such as the features section.",
+    jobTitle: "Interaction Design / Front End Development",
+    techStack: "HTML / CSS / JS",
+    background:
+      "This project was a front-end  challenge from Frontend Mentor. It’s a platform that enables you to practice building websites to a design and project brief. Each challenge includes mobile and desktop designs to show how the website should look at different screen sizes. Creating these projects has helped me refine my workflow and solve real-world coding problems. I’ve learned something new with each project, helping me to improve and adapt my style.",
+    imgMobilePreview: "/images/detail/mobile/image-bookmark-preview-1.jpg",
+    imgMobile2Preview: "/images/detail/mobile/image-bookmark-preview-2.jpg",
+    imgTabletPreview: "/images/detail/tablet/image-bookmark-preview-2.jpg",
+    imgTablet2Preview: "/images/detail/tablet/image-bookmark-preview-2.jpg",
+    imgDesktopPreview: "/images/detail/desktop/image-bookmark-preview-2.jpg",
+    imgDesktop2Preview: "/images/detail/desktop/image-bookmark-preview-2.jpg",
+  },
+  {
+    id: 3,
+    heroMobile: "/images/detail/mobile/image-insure-hero.jpg",
+    heroTablet: "/images/detail/tablet/image-insure-hero.jpg",
+    heroDesktop: "/images/detail/desktop/image-insure-hero.jpg",
+    title: "Insure",
+    intro:
+      "This project required me to build a fully responsive landing page to the designs provided. I used HTML5, along with CSS Grid and JavaScript for the areas that required interactivity, such as the features section.",
+    jobTitle: "Interaction Design / Front End Development",
+    techStack: "HTML / CSS / JS",
+    background:
+      "This project was a front-end  challenge from Frontend Mentor. It’s a platform that enables you to practice building websites to a design and project brief. Each challenge includes mobile and desktop designs to show how the website should look at different screen sizes. Creating these projects has helped me refine my workflow and solve real-world coding problems. I’ve learned something new with each project, helping me to improve and adapt my style.",
+    imgMobilePreview: "/images/detail/mobile/image-insure-preview-1.jpg",
+    imgMobile2Preview: "/images/detail/mobile/image-insure-preview-2.jpg",
+    imgTabletPreview: "/images/detail/tablet/image-insure-preview-2.jpg",
+    imgTablet2Preview: "/images/detail/tablet/image-insure-preview-2.jpg",
+    imgDesktopPreview: "/images/detail/desktop/image-insure-preview-2.jpg",
+    imgDesktop2Preview: "/images/detail/desktop/image-insure-preview-2.jpg",
+  },
+  {
+    id: 4,
+    heroMobile: "/images/detail/mobile/image-fylo-hero.jpg",
+    heroTablet: "/images/detail/tablet/image-fylo-hero.jpg",
+    heroDesktop: "/images/detail/desktop/image-fylo-hero.jpg",
+    title: "Fylo",
+    intro:
+      "This project required me to build a fully responsive landing page to the designs provided. I used HTML5, along with CSS Grid and JavaScript for the areas that required interactivity, such as the features section.",
+    jobTitle: "Interaction Design / Front End Development",
+    techStack: "HTML / CSS / JS",
+    background:
+      "This project was a front-end  challenge from Frontend Mentor. It’s a platform that enables you to practice building websites to a design and project brief. Each challenge includes mobile and desktop designs to show how the website should look at different screen sizes. Creating these projects has helped me refine my workflow and solve real-world coding problems. I’ve learned something new with each project, helping me to improve and adapt my style.",
+    imgMobilePreview: "/images/detail/mobile/image-fylo-preview-1.jpg",
+    imgMobile2Preview: "/images/detail/mobile/image-fylo-preview-2.jpg",
+    imgTabletPreview: "/images/detail/tablet/image-fylo-preview-2.jpg",
+    imgTablet2Preview: "/images/detail/tablet/image-fylo-preview-2.jpg",
+    imgDesktopPreview: "/images/detail/desktop/image-fylo-preview-2.jpg",
+    imgDesktop2Preview: "/images/detail/desktop/image-fylo-preview-2.jpg",
+  },
+];
+
+export default projectData;
