d2fa3ae4a75da35dc38c64109da8eee1db6283c4
royeradames <royeraadames@gmail.com>
2021-08-02 18:45:08 -0400

    📦import libraries

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,7 +1,12 @@
+// import library
 import React from "react";
+import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+
+
 
 const Project = (props: { project: string }) => {
   return <article className="project">Project {props.project}</article>;
-};
 
 export default Project;
