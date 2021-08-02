204139ea56a68dafddee5af4e4a7f18d84e05e47
royeradames <royeraadames@gmail.com>
2021-08-02 15:11:53 -0400

    📦recieve page name to give page specific classes

--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -2,23 +2,31 @@ import React from "react";
 import { ReactComponent as Github } from "../images/icons/github.svg";
 import { ReactComponent as Twitter } from "../images/icons/twitter.svg";
 import { ReactComponent as LinkedIn } from "../images/icons/linkedin.svg";
-const SocialMedia = (props: { footer: string }) => {
+const SocialMedia = (props: { page?: string }) => {
   return (
     <article
-      className={`social-media ${props.footer ? "footer__social-media" : ""}`}
+      className={`social-media ${
+        props.page ? `${props.page}__social-media` : ""
+      }`}
     >
       <a href="https://github.com/royeradames" target="_blank" rel="noreferrer">
-        <Github className="social-media__icon social-media__github" />
+        <Github
+          className={`social-media__icon social-media__github ${props.page}__social-media-icon`}
+        />
       </a>
       <a href="#root" target="_blank" rel="noreferrer">
-        <Twitter className="social-media__icon social-media__twitter" />
+        <Twitter
+          className={`social-media__icon social-media__github ${props.page}__social-media-icon`}
+        />
       </a>
       <a
         href="https://www.linkedin.com/in/royer-adames/"
         target="_blank"
         rel="noreferrer"
       >
-        <LinkedIn className="social-media__icon social-media__linkedin" />
+        <LinkedIn
+          className={`social-media__icon social-media__github ${props.page}__social-media-icon`}
+        />
       </a>
     </article>
   );
