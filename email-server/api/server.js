24f92b3c43ca5a7805a87d41ff71a4dcaa433811
royeradames <royeraadames@gmail.com>
2021-08-07 19:00:09 -0400

    📦import library and point to router

---
+++ email-server/api/server.js
@@ -0,0 +1,14 @@
+/*  Libraries */
+const express = require("express")
+const helmet = require("helmet")
+const cors = require("cors")
+
+/* routers */
+const emailRouter = require("./email")
+
+
+
+
+
+
+module.exports = server
