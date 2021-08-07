7298b9a1a4abfafbbd3c950e28492491100afbd6
royeradames <royeraadames@gmail.com>
2021-08-06 20:24:29 -0400

    📦show the images depending on the sizes, and the px density👌update types

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,18 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import viewPortsSize from "../viewPortsSize";
+import mediaQueries from "../viewPortsSize";
 import projectSnipeDetails from "../projectSnipeDetails";
 // component
 import ContactMeArticle from "./ContactMeArticle";
 
-type DataStructure = {
-  id: number;
-  title: string;
-  message: string;
-  imgLinkMobile: string;
-  imgLinkTablet: string;
-  imgLinkDesktop: string;
-}[];
 const Portfolio = () => {
   const pattern = (id: number) => {
     const even = id % 2 === 0;
@@ -24,27 +16,54 @@ const Portfolio = () => {
     }
   };
 
-  const portfolioList = (projectSnipeDetails: DataStructure) => {
+  const portfolioList = (
+    projectSnipeDetails: {
+      id: number;
+      title: string;
+      message: string;
+      imgLinkMobile: string;
+      imgLinkMobile2x: string;
+      imgLinkTablet: string;
+      imgLinkTablet2x: string;
+      imgLinkDesktop: string;
+      imgLinkDesktop2x: string;
+    }[]
+  ) => {
     return projectSnipeDetails.map(
       (project: {
         id: number;
-        imgLinkMobile: string;
-        imgLinkTablet: string;
-        imgLinkDesktop: string;
         title: string;
         message: string;
+        imgLinkMobile: string;
+        imgLinkMobile2x: string;
+        imgLinkTablet: string;
+        imgLinkTablet2x: string;
+        imgLinkDesktop: string;
+        imgLinkDesktop2x: string;
       }) => {
         return (
           <article
             className={`portfolio__project ${pattern(project.id)}`}
             key={project.id}
           >
-            <img
-              src={project.imgLinkMobile}
-              srcSet={`${project.imgLinkMobile} ${viewPortsSize.mobile}, ${project.imgLinkTablet} ${viewPortsSize.tablet}, ${project.imgLinkDesktop} ${viewPortsSize.desktop}`}
-              alt={`${project.title} project`}
-              className="portfolio__img"
-            />
+            {/* show the images depending on the sizes, and the px density */}
+            <picture className="portfolio__img">
+              <source
+                srcSet={`${project.imgLinkDesktop} 1x, ${project.imgLinkDesktop2x} 2x`}
+                media={mediaQueries.desktop}
+              />
+              <source
+                srcSet={`${project.imgLinkTablet} 1x, ${project.imgLinkTablet2x} 2x`}
+                media={mediaQueries.tablet}
+              />
+              <source
+                srcSet={`${project.imgLinkMobile} 1x, ${project.imgLinkMobile2x}`}
+              />
+              <img
+                src={project.imgLinkMobile}
+                alt={`${project.title} project`}
+              />
+            </picture>
             <div className="horizontal-line portfolio__horizontal-line portfolio__horizontal-line-top" />
             <h1 className="title portfolio__title">{project.title}</h1>
             <p className="portfolio__message">{project.message}</p>
