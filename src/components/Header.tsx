ecf20895eec981d80935223e688323c93269c85b
royeradames <royeraadames@gmail.com>
2021-08-06 21:10:47 -0400

    📦have a larger hit box to close the menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,22 +14,24 @@ function Header() {
     setIsMenuOpen(!isMenuOpen);
   };
   return (
-    <>
-      <header className="website__header header">
-        <Link to="/" className="header__logo">
-          <Logo />
-        </Link>
-        <Hamburger
-          className={`header__hamburger ${isMenuOpen ? "header__hide" : ""}`}
-          onClick={() => mobileMenuLogic(isMenuOpen, setIsMenuOpen)}
-        />
-        <Close
-          className={`header__close ${isMenuOpen ? "" : "header__hide"}`}
-          onClick={() => mobileMenuLogic(isMenuOpen, setIsMenuOpen)}
-        />
-        <Nav menu={`header__nav ${isMenuOpen ? " header__open" : ""}`} />
-      </header>
-    </>
+    <header className="website__header header">
+      <Link to="/" className="header__logo">
+        <Logo />
+      </Link>
+      <Hamburger
+        className={`header__hamburger ${isMenuOpen ? "header__hide" : ""}`}
+        onClick={() => mobileMenuLogic(isMenuOpen, setIsMenuOpen)}
+      />
+      <Close
+        className={`header__close ${isMenuOpen ? "" : "header__hide"}`}
+        onClick={() => mobileMenuLogic(isMenuOpen, setIsMenuOpen)}
+      />
+      <Nav menu={`header__nav ${isMenuOpen ? " header__open" : ""}`} />
+      <div
+        className={`header__close-anywhere ${isMenuOpen ? "" : "header__hide"}`}
+        onClick={() => mobileMenuLogic(isMenuOpen, setIsMenuOpen)}
+      />
+    </header>
   );
 }
 
