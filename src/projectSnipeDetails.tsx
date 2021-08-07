45f791fb9079cbe904038fb4b8dc2c01c65ada26
royeradames <royeraadames@gmail.com>
2021-08-06 20:14:42 -0400

    📦add 2x images

--- src/projectSnipeDetails.tsx
+++ src/projectSnipeDetails.tsx
@@ -5,8 +5,11 @@ const projectSnipeDetails = [
     message:
       "This project required me to build a fully responsive landing page to the designs provided. I used HTML5, along with CSS Grid and JavaScript for the areas that required interactivity, such as the testimonial slider.",
     imgLinkMobile: "./images/portfolio/mobile/image-portfolio-manage.jpg",
+    imgLinkMobile2x: "./images/portfolio/mobile/image-portfolio-manage@2.jpg",
     imgLinkTablet: "./images/portfolio/tablet/image-portfolio-manage.jpg",
+    imgLinkTablet2x: "./images/portfolio/tablet/image-portfolio-manage@2.jpg",
     imgLinkDesktop: "./images/portfolio/desktop/image-portfolio-manage.jpg",
+    imgLinkDesktop2x: "./images/portfolio/desktop/image-portfolio-manage@2.jpg",
   },
   {
     id: 2,
@@ -14,8 +17,12 @@ const projectSnipeDetails = [
     message:
       "This project required me to build a fully responsive landing page to the designs provided. I used HTML5, along with CSS Grid and JavaScript for the areas that required interactivity, such as the features section.",
     imgLinkMobile: "./images/portfolio/mobile/image-portfolio-bookmark.jpg",
+    imgLinkMobile2x: "./images/portfolio/mobile/image-portfolio-bookmark@2.jpg",
     imgLinkTablet: "./images/portfolio/tablet/image-portfolio-bookmark.jpg",
+    imgLinkTablet2x: "./images/portfolio/tablet/image-portfolio-bookmark@2.jpg",
     imgLinkDesktop: "./images/portfolio/desktop/image-portfolio-bookmark.jpg",
+    imgLinkDesktop2x:
+      "./images/portfolio/desktop/image-portfolio-bookmark@2.jpg",
   },
   {
     id: 3,
@@ -23,8 +30,11 @@ const projectSnipeDetails = [
     message:
       "This was a small project which mostly consisted of HTML and CSS. I built a fully-responsive landing page. The only JavaScript this project required was to enable the toggling of the mobile navigation.",
     imgLinkMobile: "./images/portfolio/mobile/image-portfolio-insure.jpg",
+    imgLinkMobile2x: "./images/portfolio/mobile/image-portfolio-insure@2.jpg",
     imgLinkTablet: "./images/portfolio/tablet/image-portfolio-insure.jpg",
+    imgLinkTablet2x: "./images/portfolio/tablet/image-portfolio-insure@2.jpg",
     imgLinkDesktop: "./images/portfolio/desktop/image-portfolio-insure.jpg",
+    imgLinkDesktop2x: "./images/portfolio/desktop/image-portfolio-insure@2.jpg",
   },
   {
     id: 4,
@@ -32,8 +42,11 @@ const projectSnipeDetails = [
     message:
       "This project was built in pure HTML and CSS. I had mobile and desktop designs to work to and built it so that it was fully-responsive. I took a mobile-first approach and used modern CSS like Flexbox and Grid for layout purposes.",
     imgLinkMobile: "./images/portfolio/mobile/image-portfolio-fylo.jpg",
+    imgLinkMobile2x: "./images/portfolio/mobile/image-portfolio-fylo@2.jpg",
     imgLinkTablet: "./images/portfolio/tablet/image-portfolio-fylo.jpg",
+    imgLinkTablet2x: "./images/portfolio/tablet/image-portfolio-fylo@2.jpg",
     imgLinkDesktop: "./images/portfolio/desktop/image-portfolio-fylo.jpg",
+    imgLinkDesktop2x: "./images/portfolio/desktop/image-portfolio-fylo@2.jpg",
   },
 ];
 
