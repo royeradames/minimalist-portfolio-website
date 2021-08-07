5a81fc7a2abb3c18269c5556ffe0974a3d813148
royeradames <royeraadames@gmail.com>
2021-08-07 19:00:35 -0400

    📦start server and add middleware

--- email-server/api/server.js
+++ email-server/api/server.js
@@ -6,8 +6,19 @@ const cors = require("cors")
 /* routers */
 const emailRouter = require("./email")
 
+/* Start server */
+const server = express()
 
+/* Middlewares */
+// application level
+server.use(express.json())
 
+// third party
+server.use(helmet())
+server.use(cors())
+
+// developer
+server.use("/email", emailRouter)
 
 
 
