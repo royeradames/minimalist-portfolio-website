910f98ba18ca1a3b863d2583839979df7adeaf50
royeradames <royeraadames@gmail.com>
2021-08-11 19:46:13 -0400

    📦move email server to another repo

--- email-server/index.js
+++
@@ -1,8 +0,0 @@
-const server = require("./api/server");
-
-// dynamic port
-const PORT = process.env.PORT || 5000
-
-server.listen(PORT, () => {
-  console.log(`\n=== Server listening on port ${PORT} ===\n`)
-})
