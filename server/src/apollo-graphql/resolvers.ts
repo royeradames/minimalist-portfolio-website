42a050a4e80cb625eca0ae9901018be204a426e5
royeradames <royeraadames@gmail.com>
2021-11-06 15:31:30 -0400

    📦define query resolvers

--- server/src/apollo-graphql/resolvers.ts
+++ server/src/apollo-graphql/resolvers.ts
@@ -1,5 +1,12 @@
 const resolvers = {
-    
+    Query: {
+        // get a list of projects
+        portfolio: (_, __, {dataSources}) =>  dataSources.db.getProjects(),
+        // Find a project by ID
+        projectById: (_, {id}, {dataSources}, info) => dataSources.db.getProjectById(id),
+        // Find a project by the title
+        projectByTitle: (_, {title}, {dataSources}) => dataSources.db.getProjectByTitle(title),
+    },
 }
 
 export default resolvers
