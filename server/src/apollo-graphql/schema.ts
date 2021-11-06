a759c81700e88d90a8a5f5fda0b08584f725b3e8
royeradames <royeraadames@gmail.com>
2021-11-06 15:28:10 -0400

    📦define project type

--- server/src/apollo-graphql/schema.ts
+++ server/src/apollo-graphql/schema.ts
@@ -1,6 +1,27 @@
 import {gql} from "apollo-server"
 
 const typeDefs = gql`
+    "A project is a list of project specific infromation"
+    type Project{
+        ProjectId: ID!
+        "the project title"
+        Title: String!
+        "the introduction of the project"
+        Intro: String
+        "the work title for the project"
+        JobTitle: String
+        "the background information of the project"
+        Background: String
+        "a list of technologies used in this project"
+        Techstack: String
+        "Hero image for the project detail page"
+        heroImg: Image
+        "Image of the project in desktop size"
+        desktopPreview: Image
+        "Image of the project in mobile size"
+        mobilePreview: Image
+    }
+
     "Project Images"
     type Image{
         imagesId: ID!
