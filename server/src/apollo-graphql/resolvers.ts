41d12b90508764b2e1814b299b9cbbd8d75dea20
royeradames <royeraadames@gmail.com>
2021-11-06 15:31:54 -0400

    📦define resolvers mutation

--- server/src/apollo-graphql/resolvers.ts
+++ server/src/apollo-graphql/resolvers.ts
@@ -7,6 +7,134 @@ const resolvers = {
         // Find a project by the title
         projectByTitle: (_, {title}, {dataSources}) => dataSources.db.getProjectByTitle(title),
     },
+    Mutation: {
+        // Update the project
+        updateProject: async (_, {id, title, intro, jobTitle, background, techstack}, {dataSources}) =>{ 
+            
+            try {
+                const project = await dataSources.db.updateProject({
+                ProjectID: id,
+                Title: title,
+                Intro: intro,
+                JobTitle: jobTitle,
+                Background: background,
+                techstack: techstack
+                })
+
+                return {
+                    code: 200,
+                    success: true,
+                    message: "project was successfully updated",
+                    project
+                }
+            } catch (error) {
+                return {
+                    code: 400,
+                    success: false,
+                    message: "project fail to update",
+                    project: null
+                }
+            }
+            
+        },
+        // Add new project
+        newProject: async (_, {title, intro, jobTitle, background, techstack}, {dataSources}) => {
+            try{
+                const project = await dataSources.db.newProject({
+                Title: title,
+                Intro: intro,
+                JobTitle: jobTitle,
+                Background: background,
+                techstack: techstack
+                })
+
+                return {
+                    code: 200,
+                    success: true,
+                    message: "new project was added",
+                    project
+                }
+            }catch(error){
+                return {
+                    code: 400,
+                    success: false,
+                    message: "fail to add new project"
+                }
+            }
+            
+        },
+        // delete a Project
+        deleteProject: async (_, {id}, {dataSources}) => {
+            try {
+                const project = await dataSources.db.deleteProject(id)
+
+                return {
+                    code: 200,
+                    success: true,
+                    message: "project was deleted",
+                    project
+                }
+            } catch (error) {
+                return {
+                    code: 400,
+                    success: false,
+                    message: "fail to delete project",
+                    project: null
+                }
+            }
+            
+        },
+        addProjectImages: async (_, {
+            projectId, 
+            heroImageTitle, 
+            heroImageAltText, 
+            heroImgSrc,
+            desktopImageTitle, 
+            desktopImageAltText, 
+            desktopImgSrc,
+            mobileImageTitle, 
+            mobileImageAltText, 
+            mobileImgSrc
+        }, {dataSources}) => {
+            try {
+                const ProjectId = {
+                ProjectId: projectId
+            }
+            const newImages = [
+                {
+                    ImageTitle: heroImageTitle, 
+                    ImageAltText: heroImageAltText, 
+                    ImageData: heroImgSrc,
+                },
+                {
+                    ImageTitle: desktopImageTitle, 
+                    ImageAltText: desktopImageAltText, 
+                    ImageData: desktopImgSrc,
+                },
+                {
+                    ImageTitle: mobileImageTitle, 
+                    ImageAltText: mobileImageAltText, 
+                    ImageData: mobileImgSrc,
+                },
+            ]
+            return {
+                code: 200,
+                success: true,
+                message: "the images where added the project",
+                project: await dataSources.db.addProjectImages(ProjectId, newImages)
+            }
+            } catch (error) {
+                return {
+                    code: 400,
+                    success: false,
+                    message: "fail to add images to the project",
+                    project: null
+                }
+            }
+            
+        }
+    },
+    
 }
 
 export default resolvers
