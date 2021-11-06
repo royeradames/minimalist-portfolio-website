fb3bd1c742779cb23bc5464b46965ee5b794af2a
royeradames <royeraadames@gmail.com>
2021-11-06 15:33:20 -0400

    📦innit index and import libraries

---
+++ server/src/apolloIndex.ts
@@ -0,0 +1,5 @@
+import {ApolloServer} from "apollo-server"
+import typeDefs from "./apollo-graphql/schema"
+import resolvers from "./apollo-graphql/resolvers"
+import TrackDB from "./apollo-graphql/datasources/track-db"
+
