f8266a9376bb7aad7824d86954969736ac6191e2
royeradames <royeraadames@gmail.com>
2021-11-06 15:33:38 -0400

    📦get knex config object

--- server/src/apolloIndex.ts
+++ server/src/apolloIndex.ts
@@ -3,3 +3,9 @@ import typeDefs from "./apollo-graphql/schema"
 import resolvers from "./apollo-graphql/resolvers"
 import TrackDB from "./apollo-graphql/datasources/track-db"
 
+/* knex config object */
+const knexConfig = require("./knexfile")
+const environment = process.env.DB_ENV || "development"
+
+
+
