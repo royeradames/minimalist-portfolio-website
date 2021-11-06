51fb90d85781f181a4feebe9b68cce2a9d913d53
royeradames <royeraadames@gmail.com>
2021-11-06 15:27:54 -0400

    📦define image type

--- server/src/apollo-graphql/schema.ts
+++ server/src/apollo-graphql/schema.ts
@@ -1,6 +1,16 @@
 import {gql} from "apollo-server"
 
 const typeDefs = gql`
-    
+    "Project Images"
+    type Image{
+        imagesId: ID!
+        "the image title"
+        imageTitle: String
+        "The alt text of the image"
+        imageAltText: String
+        "The project image"
+        imgSrc: String!
+    }
+
 ` 
 export default typeDefs
